3ccc7dfa84e3f87f6395b95a2f126818b323843e
TheGravvytrain <97461759+TheGravvytrain@users.noreply.github.com>
2022-01-24 15:22:06 -0600

    Add files via upload

---
+++ dateTime.js
@@ -0,0 +1,8 @@
+function getTime()
+{
+    // create a variable
+    currentDateTime = new Date();
+    // variables to hold the string representation of the current date
+    localTime = currentDateTime.toString();
+    return localTime;
+}
